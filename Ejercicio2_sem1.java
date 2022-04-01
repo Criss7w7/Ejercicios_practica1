12b2062d3c1409de4e42cc94ede76601cb95179d
Criss7w7 <101343214+Criss7w7@users.noreply.github.com>
2022-04-01 15:12:52 -0300

    Update Ejercicio2_sem1.java

--- Ejercicio2_sem1.java
+++ Ejercicio2_sem1.java
@@ -2,27 +2,27 @@ package ejercicio2_sem1;
 
 public class Ejercicio2_sem1 {
   public static void main(String[] args) {
-   //declaramos variables
-    double  Dolares, Europa, Reino_u, Australia, Canada, Usd;
+   //declaramos las variables
+    double  dolares, europa, reino_u, australia, canada, usd;
     
     //Establecemos valores a las variables
-    Dolares = 100;
-    Europa = 0.70;
-    Reino_u = 0.61;
-    Australia = 0.95;
-    Canada = 0.97;
-    Usd = 1.00;
+    dolares = 100;
+    europa = 0.70;
+    reino_u = 0.61;
+    australia = 0.95;
+    canada = 0.97;
+    usd = 1.00;
     
       //realizamos  conversion
-      double conversion1 = Dolares * Europa;
+      double conversion1 = dolares * europa;
       System.out.println("La covensercion de Dolares a la moneda de Europa es : " + conversion1); 
-      double conversion2 = Dolares * Reino_u;
+      double conversion2 = dolares * reino_u;
       System.out.println("La covensercion de Dolares a la moneda de Reino Unido es : " + conversion2); 
-      double conversion3 = Dolares * Australia;
+      double conversion3 = dolares * australia;
       System.out.println("La covensercion de dolares a la moneda de Australia es : " + conversion3); 
-      double conversion4 = Dolares * Canada;
+      double conversion4 = dolares * canada;
       System.out.println("La covensercion de dolares a la moneda de Canada es : " + conversion4); 
-      double conversion5 = Dolares * Usd;
+      double conversion5 = dolares * usd;
       System.out.println("La covensercion de dolares a la moneda  de USD es : " + conversion5); 
     
     
